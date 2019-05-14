5ba74e49be6f1086a76783f01182e8b0f27ddc31
zhangcong3 <zhangcong3@bigo.sg>
2019-05-14 06:14:00 -0700

    0514 learning

--- daliy_practice/day_0423.go
+++ daliy_practice/day_0423.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Student struct {
+	name   string
+	age    int
+	weight float32
+	score  []int
+}
+
 func main() {
-	
+	pp := new(Student) //使用 new 关键字创建一个指针
+	*pp = Student{"qishuangming", 23, 65.0, []int{2, 3, 6}}
+	fmt.Printf("stu pp have %d subjects\n", len((*pp).score))
+	*pp = Student{name: "zc"}
+	fmt.Printf("stu name %s\n", pp.name) //Go语言自带隐式解引用
+	aa := new(Student)
+	*aa = Student{name: "123"}
+	*pp = Student{name: "zc"}
+	fmt.Printf("stu name %s\n", aa.name)
+
+	a := &Student{name: "456"}
+	a.name = "999"
+	fmt.Printf("%v", a)
 }
